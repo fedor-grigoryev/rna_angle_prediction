e39add3f07ec949f145e53de294582569a31522a
ElizavetaBogdan <37368697+ElizavetaBogdan@users.noreply.github.com>
2024-01-09 18:24:35 +0300

    Update visualisation.py

--- src/visualisation.py
+++ src/visualisation.py
@@ -7,8 +7,28 @@ import matplotlib.pyplot as plt
 def visualise_distr(train_data_path):
     RNA_LIST = {i: name for i, name in enumerate(
         listdir(train_data_path))}
-    gamma = pd.read_csv(f"{train_data_path}/1CSL.csv")["gamma"]
-    plt.hist(gamma, label="1CSL gamma distribution")
+    gammas=[]
+    for elem in RNA_LIST_test:
+      gammas.append(np.array(pd.read_csv(train_data_path+RNA_LIST[elem])["gamma"]))
+    for i in range(len(gammas_test)):
+      plt.hist(gammas[i])
     plt.xlabel("Degrees")
     plt.ylabel("Number of Angles")
-    plt.show()
+    #plt.show()
+
+
+def visualise_len_distr(train_data_path):
+    RNA_LIST = {i: name for i, name in enumerate(
+        listdir(train_data_path))}
+    sequences=[]
+    for elem in RNA_LIST_test:
+      sequences.append(np.array(pd.read_csv(folder_test+RNA[elem])["base"].str[-1]))
+    seq_ln=[]
+    for i in range(len(sequences)):
+      seq=''
+      for elem in sequences[i]:
+        seq+=elem.upper()
+      seq_ln.append(len(seq))
+    plt.hist(seq_ln)
+    plt.xlabel("Length")
+    plt.ylabel("Number of Sequences")
