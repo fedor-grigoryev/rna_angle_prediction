5752515e2da7b9ddb37c7837614f2da21a5ff9ce
Fedor Grigoryev <grigoryev.fedor@gmail.com>
2024-01-10 23:04:29 +0100

    cli

---
+++ src/angles_helper.py
@@ -0,0 +1,99 @@
+import argparse
+import os
+import torch
+
+from utils import read_fasta, fasta_preprocess
+
+model2path = {
+   'binary': 'BinClassifier.pt',
+    # 'tertiary': ,
+    '20': 'TwentyClassifier.pt',
+    '30': 'ThirtyClassifier.pt',
+    'regression': 'Regressor.pt'
+}
+
+class AngleHelper:
+    def __init__(self, in_path: str, out_path: str, model_type: str):
+        self.in_path = in_path
+        self.out_path = out_path
+        self.model_type = model_type
+
+
+    def predict(self, in_path: str, out_path: str, model_type: str):
+        """
+        Function that predicts the angles for the sequence of nucleotides
+        Args:
+        - in_path: path to a `fasta` file.
+            Example:
+            "
+            >17EP
+            ACGUUCU
+            "
+        - out_path: path to a `.json` file where will be stored the prediciton.
+            It should have the following keys:
+            {
+                "17EP": {
+                "sequence": "ACGUUCU",
+                "angles": {"gamma": [0, 0, 0, 0, 0, 0, 0]}
+                }
+            }
+        - model_type: the model to be used for inference, the choice is between [`binary`, `tertiary`, `20`, `30`, `regression`]'
+        """
+
+        sequences_list = read_fasta(in_path)
+        padded_sequences, masks = fasta_preprocess(sequences_list)
+
+        model_path = os.path.join("../models", model2path[model_type])
+        model = torch.load(model_path)
+        model.eval()
+
+        # JSON NEEDED BUT JUST SAVING FOR NOW 
+        outputs = model(padded_sequences)
+        torch.save(outputs, out_path)
+        return None
+
+if __name__ == "__main__":
+    # Example of usage
+    in_path = os.path.join("data", "sample", "example.fasta")
+    out_path = "sample.json"
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_path',
+        action='store',
+        required=True,
+        type=str,
+        help='absolute path to the input fasta file'
+    )
+
+    parser.add_argument(
+        '--out_path',
+        action='store',
+        required=True,
+        type=str,
+        help='absolute path to the output .json file'
+    )
+
+    parser.add_argument(
+        '--model_type',
+        action='store',
+        required=True,
+        type=str,
+        help='model type to use for inference, the choice is between [`binary`, `tertiary`, `20`, `30`, `regression`]'
+    )
+
+
+    argv = vars(parser.parse_args())
+    args = parser.parse_args()
+
+    for key, value in argv.items():
+        print(f'{key} is {value}')
+
+    angle_helper = AngleHelper(args.input_path, args.out_path, args.model_type)
+
+    
+    angle_helper.predict(in_path, out_path)
+
+
+    
