5035b8512aee1d38ae90cac8c914dd34b42842e8
emil <33105890+poolsar42@users.noreply.github.com>
2024-01-11 16:05:44 +0100

    fix statical scores and seed the model

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import json
 from models import LSTMRegressor, LSTMClassifier
 from utils import calculate_class_index, calculate_binary_index
 from train import train_regressor, train_classifier
-from evaluate import evaluate_classifier, compare_spot_rna_1d_regressor, compare_spot_rna_1d_classifier, predict_angles, compare_spot_rna_1d_bin_stats_classifier
+from evaluate import evaluate_classifier, compare_dssr_regressor, compare_dssr_classifier, predict_angles, compare_dssr_bin_stats_classifier, compare_dssr_spot_angles
 
 if __name__ == "__main__":
     # visualise_distr("../data/angles/TrainingSet")
@@ -44,95 +44,102 @@ if __name__ == "__main__":
         [calculate_class_index(x, num_classes=30) for x in seq] for seq in padded_gammas_test]
 
 # ------------------ TRAINING AND SAVING MODELS --------------------------------
-    # # REGRESSION APPROACH
-    # print("REGRESSION APPROACH")
-    # Regressor = LSTMRegressor(
-    #     num_embeddings=4, embedding_dim=8, hidden_dim=15)
+    # REGRESSION APPROACH
+    print("REGRESSION APPROACH")
+    Regressor = LSTMRegressor(
+        num_embeddings=4, embedding_dim=8, hidden_dim=15)
 
-    # train_regressor(Regressor,
-    #                 padded_sequences_train,
-    #                 padded_gammas_train,
-    #                 masks_train)
-    # # Save the model
-    # torch.save(Regressor, "../models/Regressor.pt")
+    train_regressor(Regressor,
+                    padded_sequences_train,
+                    padded_gammas_train,
+                    masks_train)
+    # Save the model
+    torch.save(Regressor, "../models/Regressor.pt")
 
-    # # BINARY CLASSIFICATION APPROACH
-    # print("BINARY CLASSIFICATION APPROACH")
+    # BINARY CLASSIFICATION APPROACH
+    print("BINARY CLASSIFICATION APPROACH")
 
-    # BinClassifier = LSTMClassifier(
-    #     num_embeddings=4, embedding_dim=4, hidden_dim=4, num_classes=2)
+    BinClassifier = LSTMClassifier(
+        num_embeddings=4, embedding_dim=4, hidden_dim=4, num_classes=2)
 
-    # train_classifier(BinClassifier,
-    #                  num_classes=2,
-    #                  padded_sequences_train=padded_sequences_train,
-    #                  padded_gammas_train=padded_gammas_bin_classes_train,
-    #                  masks_train=masks_train,
-    #                  )
+    train_classifier(BinClassifier,
+                     num_classes=2,
+                     padded_sequences_train=padded_sequences_train,
+                     padded_gammas_train=padded_gammas_bin_classes_train,
+                     masks_train=masks_train,
+                     )
 
-    # # Save the model
-    # torch.save(BinClassifier, "../models/BinClassifier.pt")
+    # Save the model
+    torch.save(BinClassifier, "../models/BinClassifier.pt")
 
-    # # BINARY STATS CLASSIFICATION APPROACH
-    # print("BINARY STATS CLASSIFICATION APPROACH")
+    # BINARY STATS CLASSIFICATION APPROACH
+    print("BINARY STATS CLASSIFICATION APPROACH")
 
-    # BinStatsClassifier = LSTMClassifier(
-    #     num_embeddings=4, embedding_dim=4, hidden_dim=4, num_classes=2)
+    BinStatsClassifier = LSTMClassifier(
+        num_embeddings=4, embedding_dim=4, hidden_dim=4, num_classes=2)
 
-    # train_classifier(BinStatsClassifier,
-    #                  num_classes=2,
-    #                  padded_sequences_train=padded_sequences_train,
-    #                  padded_gammas_train=padded_gammas_bin_stats_classes_train,
-    #                  masks_train=masks_train,
-    #                  )
+    train_classifier(BinStatsClassifier,
+                     num_classes=2,
+                     padded_sequences_train=padded_sequences_train,
+                     padded_gammas_train=padded_gammas_bin_stats_classes_train,
+                     masks_train=masks_train,
+                     )
 
-    # # Save the model
-    # torch.save(BinStatsClassifier, "../models/BinStatsClassifier.pt")
+    # Save the model
+    torch.save(BinStatsClassifier, "../models/BinStatsClassifier.pt")
 
-    # # MULTI-CLASS CLASSIFICATION APPROACH
-    # # 20 Classes
-    # print("20 CLASSES CLASSIFICATION APPROACH")
-    # TwentyClassifier = LSTMClassifier(
-    #     num_embeddings=4, embedding_dim=8, hidden_dim=15, num_classes=20)
+    # MULTI-CLASS CLASSIFICATION APPROACH
+    # 20 Classes
+    print("20 CLASSES CLASSIFICATION APPROACH")
+    TwentyClassifier = LSTMClassifier(
+        num_embeddings=4, embedding_dim=8, hidden_dim=15, num_classes=20)
 
-    # train_classifier(TwentyClassifier,
-    #                  num_classes=20,
-    #                  padded_sequences_train=padded_sequences_train,
-    #                  padded_gammas_train=padded_gammas_20_classes_train,
-    #                  masks_train=masks_train,
-    #                  )
+    train_classifier(TwentyClassifier,
+                     num_classes=20,
+                     padded_sequences_train=padded_sequences_train,
+                     padded_gammas_train=padded_gammas_20_classes_train,
+                     masks_train=masks_train,
+                     )
 
-    # # Save the model
-    # torch.save(TwentyClassifier, "../models/TwentyClassifier.pt")
+    # Save the model
+    torch.save(TwentyClassifier, "../models/TwentyClassifier.pt")
 
-    # print("30 CLASSES CLASSIFICATION APPROACH")
-    # ThirtyClassifier = LSTMClassifier(
-    #     num_embeddings=5, embedding_dim=8, hidden_dim=15, num_classes=30)
+    print("30 CLASSES CLASSIFICATION APPROACH")
+    ThirtyClassifier = LSTMClassifier(
+        num_embeddings=5, embedding_dim=8, hidden_dim=15, num_classes=30)
 
-    # train_classifier(ThirtyClassifier,
-    #                  num_classes=30,
-    #                  padded_sequences_train=padded_sequences_train,
-    #                  padded_gammas_train=padded_gammas_30_classes_train,
-    #                  masks_train=masks_train,
-    #                  )
+    train_classifier(ThirtyClassifier,
+                     num_classes=30,
+                     padded_sequences_train=padded_sequences_train,
+                     padded_gammas_train=padded_gammas_30_classes_train,
+                     masks_train=masks_train,
+                     )
 
-    # # Save the model
-    # torch.save(ThirtyClassifier, "../models/ThirtyClassifier.pt")
+    # Save the model
+    torch.save(ThirtyClassifier, "../models/ThirtyClassifier.pt")
 
 # ------------------ LOADING MODELS AND EVALUATING -----------------------------
 
     # Load json files
 
-    # f_spot_rna_angles_train = open("../data/SPOT-RNA-1D/training.json")
-    # f_spot_rna_angles_test = open("../data/SPOT-RNA-1D/test.json")
-    f_spot_rna_angles_train = open("../data/json/train.json")
-    f_spot_rna_angles_test = open("../data/json/test.json")
+    f_spot_rna_angles_train = open("../data/SPOT-RNA-1D/training.json")
+    f_spot_rna_angles_test = open("../data/SPOT-RNA-1D/test.json")
     spot_rna_angles_train = json.load(f_spot_rna_angles_train)
     spot_rna_angles_test = json.load(f_spot_rna_angles_test)
     f_spot_rna_angles_train.close()
     f_spot_rna_angles_test.close()
 
+    f_dssr_angles_train = open("../data/json/train.json")
+    f_dssr_angles_test = open("../data/json/test.json")
+    dssr_angles_train = json.load(f_dssr_angles_train)
+    dssr_angles_test = json.load(f_dssr_angles_test)
+    f_dssr_angles_train.close()
+    f_dssr_angles_test.close()
+
     spot_rna_gammas_train = {}
     spot_rna_gammas_test = {}
+    dssr_gammas_train = {}
+    dssr_gammas_test = {}
 
     for key in spot_rna_angles_train.keys():
         spot_rna_gammas_train[spot_rna_angles_train[key]
@@ -142,6 +149,14 @@ if __name__ == "__main__":
         spot_rna_gammas_test[spot_rna_angles_test[key]
                              ['sequence']] = spot_rna_angles_test[key]['angles']['gamma']
 
+    for key in dssr_angles_train.keys():
+        dssr_gammas_train[dssr_angles_train[key]
+                          ['sequence']] = dssr_angles_train[key]['angles']['gamma']
+
+    for key in dssr_angles_test.keys():
+        dssr_gammas_test[dssr_angles_test[key]
+                         ['sequence']] = dssr_angles_test[key]['angles']['gamma']
+
     # REGRESSION APPROACH
     print("REGRESSION APPROACH")
     Regressor = torch.load("../models/Regressor.pt")
@@ -149,13 +164,13 @@ if __name__ == "__main__":
                             sequences=sequences_test,
                             num_classes=None)
 
-    compare_spot_rna_1d_regressor(Regressor,
-                                  spot_rna_gammas_train,
-                                  padded_sequences_train,
-                                  masks_train,
-                                  spot_rna_gammas_test,
-                                  padded_sequences_test,
-                                  masks_test)
+    compare_dssr_regressor(Regressor,
+                           dssr_gammas_train,
+                           padded_sequences_train,
+                           masks_train,
+                           dssr_gammas_test,
+                           padded_sequences_test,
+                           masks_test)
 
     # BINARY CLASSIFICATION APPROACH
     print("BINARY CLASSIFICATION APPROACH")
@@ -170,15 +185,15 @@ if __name__ == "__main__":
                         padded_gammas_test=padded_gammas_bin_classes_test,
                         masks_test=masks_test)
 
-    compare_spot_rna_1d_classifier(BinClassifier,
-                                   num_classes=2,
-                                   spot_rna_gammas_train=spot_rna_gammas_train,
-                                   padded_sequences_train=padded_sequences_train,
-                                   masks_train=masks_train,
-                                   spot_rna_gammas_test=spot_rna_gammas_test,
-                                   padded_sequences_test=padded_sequences_test,
-                                   masks_test=masks_test,
-                                   )
+    compare_dssr_classifier(BinClassifier,
+                            num_classes=2,
+                            dssr_gammas_train=dssr_gammas_train,
+                            padded_sequences_train=padded_sequences_train,
+                            masks_train=masks_train,
+                            dssr_gammas_test=dssr_gammas_test,
+                            padded_sequences_test=padded_sequences_test,
+                            masks_test=masks_test,
+                            )
 
     # BINARY STATS CLASSIFICATION APPROACH
     print("BINARY STATS CLASSIFICATION APPROACH")
@@ -193,14 +208,14 @@ if __name__ == "__main__":
                         padded_gammas_test=padded_gammas_bin_stats_classes_test,
                         masks_test=masks_test)
 
-    compare_spot_rna_1d_bin_stats_classifier(BinStatsClassifier,
-                                             spot_rna_gammas_train=spot_rna_gammas_train,
-                                             padded_sequences_train=padded_sequences_train,
-                                             masks_train=masks_train,
-                                             spot_rna_gammas_test=spot_rna_gammas_test,
-                                             padded_sequences_test=padded_sequences_test,
-                                             masks_test=masks_test,
-                                             )
+    compare_dssr_bin_stats_classifier(BinStatsClassifier,
+                                      dssr_gammas_train=dssr_gammas_train,
+                                      padded_sequences_train=padded_sequences_train,
+                                      masks_train=masks_train,
+                                      dssr_gammas_test=dssr_gammas_test,
+                                      padded_sequences_test=padded_sequences_test,
+                                      masks_test=masks_test,
+                                      )
 
     # MULTI-CLASS CLASSIFICATION APPROACH
     # 20 Classes
@@ -215,15 +230,15 @@ if __name__ == "__main__":
                         padded_sequences_test=padded_sequences_test,
                         padded_gammas_test=padded_gammas_20_classes_test,
                         masks_test=masks_test)
-    compare_spot_rna_1d_classifier(TwentyClassifier,
-                                   num_classes=20,
-                                   spot_rna_gammas_train=spot_rna_gammas_train,
-                                   padded_sequences_train=padded_sequences_train,
-                                   masks_train=masks_train,
-                                   spot_rna_gammas_test=spot_rna_gammas_test,
-                                   padded_sequences_test=padded_sequences_test,
-                                   masks_test=masks_test,
-                                   )
+    compare_dssr_classifier(TwentyClassifier,
+                            num_classes=20,
+                            dssr_gammas_train=dssr_gammas_train,
+                            padded_sequences_train=padded_sequences_train,
+                            masks_train=masks_train,
+                            dssr_gammas_test=dssr_gammas_test,
+                            padded_sequences_test=padded_sequences_test,
+                            masks_test=masks_test,
+                            )
 
     # 30 Classes
     print("30 CLASSES CLASSIFICATION APPROACH")
@@ -238,12 +253,18 @@ if __name__ == "__main__":
                         padded_gammas_test=padded_gammas_30_classes_test,
                         masks_test=masks_test)
 
-    compare_spot_rna_1d_classifier(ThirtyClassifier,
-                                   num_classes=30,
-                                   spot_rna_gammas_train=spot_rna_gammas_train,
-                                   padded_sequences_train=padded_sequences_train,
-                                   masks_train=masks_train,
-                                   spot_rna_gammas_test=spot_rna_gammas_test,
-                                   padded_sequences_test=padded_sequences_test,
-                                   masks_test=masks_test,
-                                   )
+    compare_dssr_classifier(ThirtyClassifier,
+                            num_classes=30,
+                            dssr_gammas_train=dssr_gammas_train,
+                            padded_sequences_train=padded_sequences_train,
+                            masks_train=masks_train,
+                            dssr_gammas_test=dssr_gammas_test,
+                            padded_sequences_test=padded_sequences_test,
+                            masks_test=masks_test,
+                            )
+
+    # Compare DSSR and SPOT-RNA
+    compare_dssr_spot_angles(dssr_gammas_train,
+                             spot_rna_gammas_train,
+                             dssr_gammas_test,
+                             spot_rna_gammas_test)
